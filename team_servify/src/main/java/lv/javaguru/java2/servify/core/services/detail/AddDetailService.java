fb74e1b80ea13ec6fb623f41e496f4cac376d9f3
AlexandrLezh <sasha_l@inbox.lv>
2023-05-04 17:19:37 +0300

    removed Optional from AddDetailService

--- team_servify/src/main/java/lv/javaguru/java2/servify/core/services/detail/AddDetailService.java
+++ team_servify/src/main/java/lv/javaguru/java2/servify/core/services/detail/AddDetailService.java
@@ -4,38 +4,32 @@ import lv.javaguru.java2.servify.core.requests.detail.AddDetailRequest;
 import lv.javaguru.java2.servify.core.responses.CoreError;
 import lv.javaguru.java2.servify.core.responses.detail.AddDetailResponse;
 import lv.javaguru.java2.servify.core.validators.AddDetailValidator;
-import lv.javaguru.java2.servify.domain.FieldTitle;
 import lv.javaguru.java2.servify.domain.PriceList;
 import lv.javaguru.java2.servify.domain.detail.Detail;
 import lv.javaguru.java2.servify.core.database.DetailDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class AddDetailService {
 
-    @Autowired private DetailDatabase database;
-    @Autowired private PriceList priceList;
-    @Autowired private AddDetailValidator validator;
+    @Autowired
+    private DetailDatabase database;
+    @Autowired
+    private PriceList priceList;
+    @Autowired
+    private AddDetailValidator validator;
 
     public AddDetailResponse execute(AddDetailRequest request) {
         List<CoreError> errors = validator.validate(request);
         if (!errors.isEmpty()) {
             return new AddDetailResponse(errors);
         }
-        Optional<Detail> detailOptional = priceList.findById(request.getId());
-        if (detailOptional.isEmpty()) {
-            errors.add(new CoreError(FieldTitle.ID, "Not valid ID"));
-            return new AddDetailResponse(errors);
-        }
-        Detail newDetail = new Detail(detailOptional.get().getType(),
-                                      detailOptional.get().getSide(),
-                                      detailOptional.get().getPrice());
+        Detail newDetail = new Detail(priceList.findById(request.getId()).get().getType(),
+                                      priceList.findById(request.getId()).get().getSide(),
+                                      priceList.findById(request.getId()).get().getPrice());
         database.save(newDetail);
         return new AddDetailResponse(newDetail);
     }
-
 }
