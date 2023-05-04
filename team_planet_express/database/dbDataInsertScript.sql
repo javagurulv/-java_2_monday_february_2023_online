08ddd8d8adb7a703b7fc2dc06a40d0656064d83d
Vsevolod <mrehot@gmail.com>
2023-05-04 20:45:58 +0300

    db

---
+++ team_planet_express/database/dbDataInsertScript.sql
@@ -0,0 +1,6 @@
+INSERT INTO shop_user(name, login, password, role)
+VALUES ("Guest", "", "", "GUEST"),
+        ("Customer", "customer", "customer", "CUSTOMER"),
+        ("Manager", "manager", "manager", "MANAGER"),
+        ("Admin", "admin", "admin", "ADMIN");
+
