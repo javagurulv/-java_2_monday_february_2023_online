44fecd545d12b174d2288c538e8236857fb83e32
Dmitrijs Kozlovs <dmitrijs.a.kozlovs@gmail.com>
2023-05-25 11:26:47 +0300

    Moved package files, changed class Exercise

--- team_fitness_app/src/main/java/lv/fitness_app/core/domain/Exercise.java
+++ team_fitness_app/src/main/java/lv/fitness_app/core/domain/Exercise.java
@@ -4,38 +4,100 @@ import java.util.Objects;
 
 public class Exercise {
 
-    private final String name;
-    private final MuscleGroup muscleGroup;
-    private final Difficulty difficulty;
-    private final Type type;
-    private final Equipment equipment;
+    private String name;
+    private String muscleGroup;
+    private String DetailedMuscleGroup;
+    private String OtherMuscleGroup;
+    private Type type;
+    private String mechanics;
+    private String equipment;
+    private Difficulty difficulty;
+    private String description;
 
-    Exercise(ExerciseBuilder builder) {
-        this.name = builder.name;
-        this.muscleGroup = builder.muscleGroup;
-        this.difficulty = builder.difficulty;
-        this.type = builder.type;
-        this.equipment = builder.equipment;
+    public Exercise() { }
+
+    public Exercise(String name, String muscleGroup, String detailedMuscleGroup, String otherMuscleGroup, Type type, String mechanics, String equipment, Difficulty difficulty, String description) {
+        this.name = name;
+        this.muscleGroup = muscleGroup;
+        DetailedMuscleGroup = detailedMuscleGroup;
+        OtherMuscleGroup = otherMuscleGroup;
+        this.type = type;
+        this.mechanics = mechanics;
+        this.equipment = equipment;
+        this.difficulty = difficulty;
+        this.description = description;
     }
 
     public String getName() {
         return name;
     }
 
-    public MuscleGroup getMuscleGroup() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMuscleGroup() {
         return muscleGroup;
     }
 
-    public Difficulty getDifficulty() {
-        return difficulty;
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    public String getDetailedMuscleGroup() {
+        return DetailedMuscleGroup;
+    }
+
+    public void setDetailedMuscleGroup(String detailedMuscleGroup) {
+        DetailedMuscleGroup = detailedMuscleGroup;
+    }
+
+    public String getOtherMuscleGroup() {
+        return OtherMuscleGroup;
+    }
+
+    public void setOtherMuscleGroup(String otherMuscleGroup) {
+        OtherMuscleGroup = otherMuscleGroup;
     }
 
     public Type getType() {
         return type;
     }
 
-    public Equipment getEquipment() {
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getMechanics() {
+        return mechanics;
+    }
+
+    public void setMechanics(String mechanics) {
+        this.mechanics = mechanics;
+    }
+
+    public String getEquipment() {
         return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
@@ -43,55 +105,26 @@ public class Exercise {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Exercise exercise = (Exercise) o;
-        return Objects.equals(name, exercise.name) && muscleGroup == exercise.muscleGroup && difficulty == exercise.difficulty && type == exercise.type && equipment == exercise.equipment;
+        return Objects.equals(name, exercise.name) && Objects.equals(muscleGroup, exercise.muscleGroup) && Objects.equals(DetailedMuscleGroup, exercise.DetailedMuscleGroup) && Objects.equals(OtherMuscleGroup, exercise.OtherMuscleGroup) && type == exercise.type && Objects.equals(mechanics, exercise.mechanics) && Objects.equals(equipment, exercise.equipment) && difficulty == exercise.difficulty && Objects.equals(description, exercise.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, muscleGroup, difficulty, type, equipment);
+        return Objects.hash(name, muscleGroup, DetailedMuscleGroup, OtherMuscleGroup, type, mechanics, equipment, difficulty, description);
     }
 
     @Override
     public String toString() {
         return "Exercise{" +
                 "name='" + name + '\'' +
-                ", muscleGroup=" + muscleGroup +
-                ", difficulty=" + difficulty +
+                ", muscleGroup='" + muscleGroup + '\'' +
+                ", DetailedMuscleGroup='" + DetailedMuscleGroup + '\'' +
+                ", OtherMuscleGroup='" + OtherMuscleGroup + '\'' +
                 ", type=" + type +
-                ", equipment=" + equipment +
+                ", mechanics='" + mechanics + '\'' +
+                ", equipment='" + equipment + '\'' +
+                ", difficulty=" + difficulty +
+                ", description='" + description + '\'' +
                 '}';
-    }
-
-    public static class ExerciseBuilder {
-
-        private final String name;
-        private final MuscleGroup muscleGroup;
-        private Difficulty difficulty;
-        private Type type;
-        private Equipment equipment;
-
-        public ExerciseBuilder(String name, MuscleGroup muscleGroup) {
-            this.name = name;
-            this.muscleGroup = muscleGroup;
-        }
-
-        public ExerciseBuilder Difficulty(Difficulty difficulty) {
-            this.difficulty = difficulty;
-            return this;
-        }
-
-        public ExerciseBuilder Type(Type type) {
-            this.type = type;
-            return this;
-        }
-
-        public ExerciseBuilder Equipment(Equipment equipment) {
-            this.equipment = equipment;
-            return this;
-        }
-
-        public Exercise build() {
-            return new Exercise(this);
-        }
     }
 }
