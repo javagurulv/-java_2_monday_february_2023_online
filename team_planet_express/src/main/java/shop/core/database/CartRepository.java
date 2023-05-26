dbda7b03f961359e66bb88c551b4b757e24750e7
arthurkov98 <30414869+arthurkov98@users.noreply.github.com>
2023-05-26 19:37:49 +0300

    delete JpaRepository

--- team_planet_express/src/main/java/shop/core/database/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package shop.core.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import shop.core.domain.cart.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long>, JpaSpecificationExecutor<Cart> {
-
-}
--- team_planet_express/src/main/java/shop/core/database/orm/CartDatabaseImpl.java
+++ team_planet_express/src/main/java/shop/core/database/orm/CartDatabaseImpl.java
@@ -3,28 +3,25 @@ package shop.core.database.orm;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import shop.core.database.CartDatabase;
-import shop.core.database.CartRepository;
 import shop.core.domain.cart.Cart;
 import shop.core.domain.cart.CartStatus;
+import shop.core.domain.cart.Cart_;
 import shop.core.domain.user.User;
 
 import java.util.List;
 import java.util.Optional;
-
-import static shop.core.database.orm.specification.CartSpecification.isCartOpen;
-import static shop.core.database.orm.specification.CartSpecification.isEqualUser;
 
 @Repository
 @Transactional
 public class CartDatabaseImpl implements CartDatabase {
     @PersistenceContext
     private EntityManager entityManager;
-    @Autowired
-    private CartRepository cartRepository;
 
     @Override
     public Cart save(Cart cart) {
@@ -34,8 +31,14 @@ public class CartDatabaseImpl implements CartDatabase {
 
     @Override
     public Optional<Cart> findOpenCartForUserId(Long userId) {
-        User reference = entityManager.getReference(User.class, userId);
-        return cartRepository.findOne(isCartOpen().and(isEqualUser(reference)));
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cart> cr = cb.createQuery(Cart.class);
+        Root<Cart> root = cr.from(Cart.class);
+        cr.select(root).where(
+                cb.equal(root.get("user"), entityManager.getReference(User.class, userId)),
+                cb.equal(root.get(Cart_.status), CartStatus.OPEN)
+        );
+        return entityManager.createQuery(cr).getResultStream().findFirst();
     }
 
     @Override
