195ee0c2a8674a4a633ea05624ccc89d5f3d4532
Pomog <thoryur@gmail.com>
2023-05-28 23:27:26 +0300

    Lesson 10, task 11, NO yield, user, internal code JOIN

--- team_eln/src/database/dbJoinScript.sql
+++ team_eln/src/database/dbJoinScript.sql
@@ -1,3 +1,19 @@
 SELECT *
 FROM ReactionData
 INNER JOIN StructureData ON ReactionData.structure_mainProduct_id = StructureData.id;
+
+
+SELECT *
+FROM structuredata
+INNER JOIN reactionstartingmaterial ON reactionstartingmaterial.structure_id = StructureData.id;
+
+SELECT *
+FROM ReactionData
+INNER JOIN StructureData ON ReactionData.structure_mainProduct_id = StructureData.id;
+
+SELECT * FROM reactiondata
+INNER join (
+SELECT *
+FROM structuredata
+INNER JOIN reactionstartingmaterial ON reactionstartingmaterial.structure_id = StructureData.id) as sm
+on reactiondata.id = sm.reaction_id
