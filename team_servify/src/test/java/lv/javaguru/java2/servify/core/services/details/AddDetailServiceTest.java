21bbae5cb4a608c93b14e4a668982516134cc6f2
azure <azureon94@gmail.com>
2023-05-29 14:34:02 +0300

    test#1

---
+++ team_servify/src/test/java/lv/javaguru/java2/servify/core/services/details/AddDetailServiceTest.java
@@ -0,0 +1,119 @@
+package lv.javaguru.java2.servify.core.services.details;
+
+import lv.javaguru.java2.servify.core.database.DetailRepository;
+import lv.javaguru.java2.servify.core.domain.FieldTitle;
+import lv.javaguru.java2.servify.core.dto.requests.AddDetailRequest;
+import lv.javaguru.java2.servify.core.dto.responses.AddDetailResponse;
+import lv.javaguru.java2.servify.core.dto.responses.CoreError;
+import lv.javaguru.java2.servify.core.services.validators.AddDetailRequestValidator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddDetailServiceTest {
+
+    @Mock private DetailRepository detailRepository;
+    @Mock private AddDetailRequestValidator validator;
+    @InjectMocks private AddDetailService service;
+
+    @Test
+    public void shouldReturnResponseWithErrorsWhenValidationFailsA() {
+        AddDetailRequest notValidRequest = new AddDetailRequest(null, "Side", BigDecimal.ZERO);
+        when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError(FieldTitle.DETAIL_TYPE, "Not valid Type")));
+        AddDetailResponse response = service.execute(notValidRequest);
+        assertTrue(response.hasErrors());
+    }
+
+    @Test
+    public void shouldReturnResponseWithErrorsWhenValidationFailsB() {
+        AddDetailRequest notValidRequest = new AddDetailRequest("Type", null, BigDecimal.ZERO);
+        when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError(FieldTitle.DETAIL_SIDE, "Not valid Side")));
+        AddDetailResponse response = service.execute(notValidRequest);
+        assertTrue(response.hasErrors());
+    }
+
+    @Test
+    public void shouldReturnResponseWithErrorsWhenValidationFailsC() {
+        AddDetailRequest notValidRequest = new AddDetailRequest("Type", "Side", null);
+        when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError(FieldTitle.DETAIL_PRICE, "Not valid Price")));
+        AddDetailResponse response = service.execute(notValidRequest);
+        assertTrue(response.hasErrors());
+    }
+
+    @Test
+    public void shouldReturnResponseWithErrorsReceivedFromValidatorA() {
+        AddDetailRequest notValidRequest = new AddDetailRequest(null, "Side", BigDecimal.ZERO);
+        when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError(FieldTitle.DETAIL_TYPE, "Not valid Type")));
+        AddDetailResponse response = service.execute(notValidRequest);
+        assertEquals(response.getErrors().size(), 1);
+        assertEquals(response.getErrors().get(0).getField(), FieldTitle.DETAIL_TYPE);
+        assertEquals(response.getErrors().get(0).getMessage(), "Not valid Type");
+    }
+
+    @Test
+    public void shouldReturnResponseWithErrorsReceivedFromValidatorB() {
+        AddDetailRequest notValidRequest = new AddDetailRequest("Type", null, BigDecimal.ZERO);
+        when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError(FieldTitle.DETAIL_SIDE, "Not valid Side")));
+        AddDetailResponse response = service.execute(notValidRequest);
+        assertEquals(response.getErrors().size(), 1);
+        assertEquals(response.getErrors().get(0).getField(), FieldTitle.DETAIL_SIDE);
+        assertEquals(response.getErrors().get(0).getMessage(), "Not valid Side");
+    }
+
+    @Test
+    public void shouldReturnResponseWithErrorsReceivedFromValidatorC() {
+        AddDetailRequest notValidRequest = new AddDetailRequest("Type", "Side", null);
+        when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError(FieldTitle.DETAIL_PRICE, "Must not be empty!")));
+        AddDetailResponse response = service.execute(notValidRequest);
+        assertEquals(response.getErrors().size(), 1);
+        assertEquals(response.getErrors().get(0).getField(), FieldTitle.DETAIL_PRICE);
+        assertEquals(response.getErrors().get(0).getMessage(), "Must not be empty!");
+    }
+
+    @Test
+    public void shouldNotInvokeDatabaseWhenRequestValidationFailsA() {
+        AddDetailRequest notValidRequest = new AddDetailRequest(null, "Side", BigDecimal.ZERO);
+        when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError(FieldTitle.DETAIL_TYPE, "Not valid Type")));
+        service.execute(notValidRequest);
+        verifyNoInteractions(detailRepository);
+    }
+//
+//    @Test
+//    public void shouldAddBookToDatabaseWhenRequestIsValid() {
+//        AddBookRequest validRequest = new AddBookRequest("Title", "Author");
+//        when(validator.validate(validRequest)).thenReturn(List.of());
+//        service.execute(validRequest);
+//        verify(bookRepository).save(argThat(new BookMatcher("Title", "Author")));
+//    }
+//
+//    @Test
+//    public void shouldReturnResponseWithoutErrorsWhenRequestIsValid() {
+//        AddBookRequest validRequest = new AddBookRequest("Title", "Author");
+//        when(validator.validate(validRequest)).thenReturn(List.of());
+//        AddBookResponse response = service.execute(validRequest);
+//        assertFalse(response.hasErrors());
+//    }
+//
+//    @Test
+//    public void shouldReturnResponseWithBookWhenRequestIsValid() {
+//        AddBookRequest validRequest = new AddBookRequest("Title", "Author");
+//        when(validator.validate(validRequest)).thenReturn(List.of());
+//        AddBookResponse response = service.execute(validRequest);
+//        assertNotNull(response.getNewBook());
+//        assertEquals(response.getNewBook().getTitle(), validRequest.getTitle());
+//        assertEquals(response.getNewBook().getAuthor(), validRequest.getAuthor());
+//    }
+
+}
