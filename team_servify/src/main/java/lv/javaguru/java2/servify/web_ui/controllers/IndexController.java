637426c679144b1b6cba0142bb1202faa4cbd755
AlexandrLezh <sasha_l@inbox.lv>
2023-05-31 07:41:11 +0300

    bug fixes

--- team_servify/src/main/java/lv/javaguru/java2/servify/web_ui/controllers/IndexController.java
+++ team_servify/src/main/java/lv/javaguru/java2/servify/web_ui/controllers/IndexController.java
@@ -1,7 +1,8 @@
 package lv.javaguru.java2.servify.web_ui.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class IndexController {
     @GetMapping(value = "/")
     public String index() {
