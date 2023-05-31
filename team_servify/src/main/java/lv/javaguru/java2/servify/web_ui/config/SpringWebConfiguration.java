e199150addf7909307288b0108bde8a27e7c4f57
AlexandrLezh <sasha_l@inbox.lv>
2023-05-31 10:28:08 +0300

    Spring security

--- team_servify/src/main/java/lv/javaguru/java2/servify/web_ui/config/SpringWebConfiguration.java
+++ team_servify/src/main/java/lv/javaguru/java2/servify/web_ui/config/SpringWebConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -13,6 +14,10 @@ import org.thymeleaf.templatemode.TemplateMode;
 @EnableWebMvc
 @ComponentScan(basePackages = {"lv.javaguru.java2.servify"})
 public class SpringWebConfiguration implements WebMvcConfigurer{
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
