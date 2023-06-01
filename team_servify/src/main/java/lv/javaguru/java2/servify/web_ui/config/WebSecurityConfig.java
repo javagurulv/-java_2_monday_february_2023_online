e02eb369c4e3d43ce0639c4e05a670e8d4f616dc
AlexandrLezh <sasha_l@inbox.lv>
2023-06-01 12:16:43 +0300

    Added Detail bug fix for Security Config

--- team_servify/src/main/java/lv/javaguru/java2/servify/web_ui/config/WebSecurityConfig.java
+++ team_servify/src/main/java/lv/javaguru/java2/servify/web_ui/config/WebSecurityConfig.java
@@ -2,43 +2,40 @@ package lv.javaguru.java2.servify.web_ui.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
     @Bean
+    @Order(1)
+    public SecurityFilterChain addDetailSecurityChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher(AntPathRequestMatcher.antMatcher("/addDetail/**"))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(AntPathRequestMatcher.antMatcher("/addDetail/**")).permitAll();
+                })
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/addDetail/**")))
+                .build();
+    }
+    @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
-                        .anyRequest().authenticated()
+        return http
+                .authorizeHttpRequests(auth -> {
+                            auth.requestMatchers("/").permitAll();
+                            auth.requestMatchers("/errors").permitAll();
+                            auth.anyRequest().authenticated();
+                        }
                 )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
-
-        return http.build();
+                .formLogin(withDefaults())
+                .logout((logout) -> logout.permitAll())
+                .build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("u")
-                        .password("1")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
 }
