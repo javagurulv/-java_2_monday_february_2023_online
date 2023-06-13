e61695caf65db0619abf15e3a67cd5694f738781
arthurkov98 <30414869+arthurkov98@users.noreply.github.com>
2023-06-13 16:43:28 +0300

    fix empty web search

---
+++ team_planet_express/src/main/java/shop/core/services/actions/customer/BuyCartServiceImpl.java
@@ -0,0 +1,11 @@
+package shop.core.services.actions.customer;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BuyCartServiceImpl {
+    //TODO for localStorage
+
+}
